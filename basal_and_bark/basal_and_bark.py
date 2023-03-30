c68e80ac66a78192ae6f3e4e6ce88fdda7a25ac6
ZachDorm <zach.dorminey@gmail.com>
2023-03-30 10:39:05 -0400

    Get notebooks links to work

--- basal_and_bark/basal_and_bark.py
+++ basal_and_bark/basal_and_bark.py
@@ -2,6 +2,7 @@
 
 import string
 import random
+import ipyleaflet
 
 def generate_random_string(length):
     letters = string.ascii_lowercase
@@ -22,3 +23,8 @@ def generate_lucky_number(length=1):
     numbers = ['1','2','3','4','5','6','7','8','9','0']
     result_str = ''.join(random.choice(numbers) for i in range(length))
     return result_str
+
+
+class Map(ipyleaflet.Map):
+    def __init__(self, center, zoom, **kwargs):
+        super().__init__(center = center, zoom = zoom, **kwargs)
