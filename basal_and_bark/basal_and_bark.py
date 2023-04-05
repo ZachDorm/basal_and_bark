28603a9fb33c1d80ae4169c7e4e7a71a2d2f8df1
ZachDorm <zach.dorminey@gmail.com>
2023-04-04 21:57:38 -0400

    Add vector functionality and add sample data

--- basal_and_bark/basal_and_bark.py
+++ basal_and_bark/basal_and_bark.py
@@ -3,13 +3,23 @@
 import string
 import random
 import ipyleaflet
+from ipyleaflet import GeoData, LayersControl
+
+import geopandas
 
 def generate_random_string(length):
+    """Generates a random string
+
+    Args:
+        length (int): length of string of random characters
+
+    Returns:
+        string: string of random characters of specified length
+    """    
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
-    """_summary_
-    """
+
 
 def generate_lucky_number(length=1):
     """Generates a lucky number of given length
@@ -34,8 +44,88 @@ def generate_lucky_number(length=1):
 #     marker=marker
 #     ))
 
+def add_layer_control(m):
+    """Add a layer controls to a basal_and_bark map
+
+    Args:
+        m (basal_and_bark map): The map to add layer controls to
+
+    Returns:
+        basal_and_bark map: The map with layer controls added to it
+    """    
+    try:
+        control = LayersControl(position='topright')
+        m.add_control(control)
+        return m
+    
+    except:
+        return None
+
+def view_data(data, **kwargs):
+    """View and explore data without adding it to a map
+
+    Args:
+        data (shp file): spatial data to be viewed
+
+    Returns:
+        ipyleaflet map: A map viewport for viewing and exploring data
+    """    
+    try:
+        add_gdf = geopandas.read_file(data)
+        return add_gdf.explore()
+    
+    except:
+        return None
+    
+
+def add_data(map, data, **kwargs):
+    """Add a shapefile to a map
+
+    Args:
+        map (basal_and_bark map): This will be the map that is being worked on
+        data (any file type that is accepted by GeoData): Originally intended for vector files
+
+    Returns:
+        basal_and_bark map: basal_and_bark map with provided data added to it
+    """    
+    try:
+        #m = Map(center = [40,-100], zoom = 4, test = "test", scroll_wheel_zoom = True)
+        f = geopandas.read_file(data)
+        geo = GeoData(geo_dataframe=f, name="TN Counties")
+        m = map.add_layer(geo)
+        return m
+    
+    except:
+        return None
+    
+
+
+def summarize_data(data, type="mean"):
+    """summarize a list of numbers using the specified method
+
+    Args:
+        data (list of int): list of numbers to be summarized
+        type (str, optional): Method for summarization. Defaults to "mean".
+
+    Returns:
+        int: summary statistic
+    """
+    if type == "max":
+        return max(data)
+    elif type == "min":
+        return min(data)
+    elif type == "sum":
+        return sum(data)
+    
+    return sum(data)/len(data)
+
 
 class Map(ipyleaflet.Map):
+    """create the Map class of basal_and_bark
+
+    Args:
+        ipyleaflet (Map): This is an ipyleaflet Map instance upon which basal_and_bark's functionality is built on
+    """    
     def __init__(self, center, zoom, **kwargs):
         if "scroll_wheel_zoom" not in kwargs:
             kwargs["scroll+wheel_zoom"]=True
