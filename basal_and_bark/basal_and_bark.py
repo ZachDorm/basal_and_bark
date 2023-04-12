c22a1d0732dce065c14bcc9e964e69fb05552485
ZachDorm <zach.dorminey@gmail.com>
2023-04-12 12:09:05 -0400

    Folium and ipyleaflet functionality

--- basal_and_bark/basal_and_bark.py
+++ basal_and_bark/basal_and_bark.py
@@ -5,9 +5,11 @@ import random
 import ipyleaflet
 from ipyleaflet import GeoData, LayersControl, GeoJSON
 import folium
+from folium import TileLayer
 import xyzservices.providers as xyz
 
 import geopandas
+from geopandas import GeoDataFrame, GeoSeries
 
 def generate_random_string(length):
     """Generates a random string
@@ -131,8 +133,8 @@ class Map(ipyleaflet.Map):
             #m = Map(center = [40,-100], zoom = 4, test = "test", scroll_wheel_zoom = True)
             f = geopandas.read_file(data)
             geo = GeoData(geo_dataframe=f, name="TN Counties")
-            m = self.add_layer(geo)
-            return m
+            self.add_layer(geo)
+            return self
     
         except:
             return None
@@ -141,19 +143,68 @@ class Map(ipyleaflet.Map):
     def add_geojson(self, data, **kwargs):
         try:
             #m = Map(center = [40,-100], zoom = 4, test = "test", scroll_wheel_zoom = True)
-            f = geopandas.read_file(data)
-            geo = GeoJSON(geo_dataframe=f, name="TN Counties")
-            m = self.add_layer(geo)
-            return m
+            self.add_shp(data)
+            # f = geopandas.read_file(data)
+            # geo = GeoData(geo_dataframe=f, name="TN Counties")
+            # self.add_layer(geo)
+            # return self
     
         except:
             return None
         
-    def add_vector(map, data, **kwargs):
-        return None  
+    def add_vector(self, data, **kwargs):
+        """Accepts a file. Checks if it is a geopandas supported format. If not, then except silently.
+
+        Args:
+            data (vector data): Geopandas supported vector format.
+
+        Returns:
+            basal_and_bark map: basal_and_bark map with the vector data added.
+        """     
+        count = 0
+
+        try:
+            self.add_shp(data)
+        except:
+            count = count +1
+        try:
+            geo = GeoData(geo_dataframe=data, name="New Data")
+            self.add_layer(geo)
+        except:
+            count = count +1
+
+        try:
+            geo = GeoSeries(data=data, name="New Data")
+            self.add_layer(geo)
+        except:
+            count = count +1
         
+        try:
+            geo = GeoDataFrame(data=data, name="New Data")
+            self.add_layer(geo)
+        except:
+            count = count +1
+
+        if(count==3):
+            return "Not a supported file type"
+        else:
+            pass
+            #raise ValueError(f"Not found.")
+        #return self
 
     def add_basemap(self, url = xyz.Esri.WorldImagery.build_url(), basemap="Esri.WorldImagery", **kwargs):
+        """Add a basemap from xyz.services
+
+        Args:
+            url (string, optional: URL to xyz.services map. Defaults to xyz.Esri.WorldImagery.build_url().
+            basemap (str, optional): Name of the basemap on xyz.services. Defaults to "Esri.WorldImagery".
+
+        Raises:
+            ValueError: If basemap does not exist.
+
+        Returns:
+            basal_and_bark map: basal_and_bark map with new basemap
+        """        
         try:
             basemap = eval(f"xyz.{basemap}")
             url = basemap.build_url()
@@ -162,7 +213,7 @@ class Map(ipyleaflet.Map):
             return b
 
         except:
-            return None
+            raise ValueError(f"Basemap '{basemap}' not found.")
         
     def add_tile_layer(self, url, name, attribution="", **kwargs):
         """Adds a tile layer to the map.
@@ -183,33 +234,63 @@ class Map(ipyleaflet.Map):
 
 
 class Map_Folium(folium.Map):
-    """create the Map class of basal_and_bark
+    """create the Map_Folium class of basal_and_bark
 
     Args:
-        ipyleaflet (Map): This is an ipyleaflet Map instance upon which basal_and_bark's functionality is built on
+        folium (Map): This is a folium Map instance upon which basal_and_bark's functionality is built on
     """    
     def __init__(self, location, tiles, zoom_start, **kwargs):
         super().__init__(location=location, tiles=tiles, zoom_start=zoom_start, **kwargs)
 
-    def add_tile_layer(self, url, name, attribution="", **kwargs):
+    def add_tile_layer_folium(self, url = xyz.Esri.WorldImagery.build_url(), name="", attribution="Esri.WorldImagery", **kwargs):
         """Adds a tile layer to the map.
         Args:
             url (str): The URL of the tile layer.
             name (str): The name of the tile layer.
             attribution (str, optional): The attribution of the tile layer. Defaults to "".
         """
-        tile_layer = ipyleaflet.TileLayer(
-            url=url,
+        tile_layer = folium.TileLayer(
+            tiles=url,
+            attr=attribution,
             name=name,
-            attribution=attribution,
             **kwargs
         )
-        self.add_layer(tile_layer)
+        tile_layer.add_to(self)
+
+    def add_basemap(self, url = xyz.Esri.WorldImagery.build_url(), basemap="Esri.WorldImagery", **kwargs):
+        """Add a basemap from xyz.services
+
+        Args:
+            url (string, optional: URL to xyz.services map. Defaults to xyz.Esri.WorldImagery.build_url().
+            basemap (str, optional): Name of the basemap on xyz.services. Defaults to "Esri.WorldImagery".
+
+        Raises:
+            ValueError: If basemap does not exist.
+
+        Returns:
+            basal_and_bark map_folium: basal_and_bark map_folium with new basemap
+        """        
+        try:
+            basemap = eval(f"xyz.{basemap}")
+            url = basemap.build_url()
+            attribution = basemap.attribution
+            b = self.add_tile_layer_folium(url, name = basemap.name, attribution=attribution, **kwargs)
+            return b
+
+        except:
+            raise ValueError(f"Basemap '{basemap}' not found.")
 
 
     def add_raster(self, url, name="raster", fit_bound = True, **kwargs):
+        """Adds a raster to the basal_and_bark map_folium
+
+        Args:
+            url (string): URL to raster you want to use.
+            name (str, optional): Name of the raster. Defaults to "raster".
+            fit_bound (bool, optional): Whether the bound of the map should be fit to the raster. Defaults to True.
+        """        
         import httpx
-        titiler_endpoint = "https://titiler.xyz"  # Developmentseed Demo endpoint. Please be kind.
+        titiler_endpoint = "https://titiler.xyz"  # Developmentseed Demo endpoint. Please be kind
 
         r = httpx.get(f"{titiler_endpoint}/cog/tilejson.json", params = {"url": url,}).json()
         bounds = r["bounds"]
@@ -218,47 +299,89 @@ class Map_Folium(folium.Map):
         tile = r["tiles"][0]
         self.add_tile_layer(url=tile, name=name, **kwargs)
 
-    def add_geojson_folium(map, data, **kwargs):
+    def add_geojson_folium(self, data, **kwargs):
+        """Add GeoJSON to a map_folium
+
+        Args:
+            data (GeoJSON file): GeoJSON
+
+        Returns:
+            basal_and_bark map_folium: basal_and_bark map_folium with provided data added to it
+        """        
         try:
-            #m = Map(center = [40,-100], zoom = 4, test = "test", scroll_wheel_zoom = True)
             f = geopandas.read_file(data)
             geo = folium.GeoJson(data=data, name="TN Counties")
-            m = geo.add_to(map)
+            m = geo.add_to(self)
             return m
     
         except:
             return None
         
 
-    def add_shp_folium(map, data, **kwargs):
-        """Add a shapefile to a map
+    def add_shp_folium(self, data, **kwargs):
+        """Add a shapefile to a map_folium
 
     Args:
-        map (basal_and_bark map): This will be the map that is being worked on
+        map_folium (basal_and_bark map_folium): This will be the map that is being worked on
         data (any file type that is accepted by GeoData): Originally intended for vector files
 
     Returns:
-        basal_and_bark map: basal_and_bark map with provided data added to it
+        basal_and_bark map_folium: basal_and_bark map_folium with provided data added to it
     """  
-        m = map 
         data=data 
         try:
-            #m = Map(center = [40,-100], zoom = 4, test = "test", scroll_wheel_zoom = True)
             f = geopandas.read_file(data)
+            geo = f["geometry"].simplify(tolerance=0.001)
+            json = geo.to_json()
+            geo_j = folium.GeoJson(data=json)
+            geo_j.add_to(self)
         
-            for _, r in f.iterrows():
-        # Without simplifying the representation of each borough,
-        # the map might not be displayed
-             sim_geo = geopandas.GeoSeries(r['geometry']).simplify(tolerance=0.001)
-            geo_j = sim_geo.to_json()
-            geo_j = folium.GeoJson(data=geo_j)
-            geo_j.add_to(map)
-        # geo = GeoData(geo_dataframe=f, name="TN Counties")
-        # m = map.add_layer(geo)
-            m
+        #     for _, r in f.iterrows():
+        # # Without simplifying the representation of each borough,
+        # # the map might not be displayed
+        #      sim_geo = geopandas.GeoSeries(r['geometry'])#.simplify(tolerance=0.001)
+        #     geo_j = sim_geo.to_json()
+        #     geo_j = folium.GeoJson(data=geo_j)
+        #     geo_j.add_to(self)
+        # # geo = GeoData(geo_dataframe=f, name="TN Counties")
+        # # m = map.add_layer(geo)
+            self
     
         except:
             None
+
+
+    def add_vector_folium(self, data, **kwargs):
+        """Accepts a file. Checks if it is a geopandas supported format. If not, then except silently.
+
+        Args:
+            data (vector data): Geopandas supported vector format.
+
+        Returns:
+            basal_and_bark map_folium: basal_and_bark map_folium with the vector data added.
+        """        
+        count = 0
+
+        try:
+            self.add_shp_folium(data)
+        except:
+            count = count +1
+        try:
+            self.add_geojson_folium(data)
+        except:
+            count = count +1
+        
+        try:
+            json = data.to_json()
+            geo_j = folium.GeoJson(data=json)
+            geo_j.add_to(self)
+        except:
+            count = count +1
+
+        if(count==3):
+            return "Not a supported file type"
+        else:
+            pass
 
 
 
